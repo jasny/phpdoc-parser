91a2711c0d7ea43441fe10ce9fe8ce383c17ee04
Sergey Boltonosov <minstel@yandex.ru>
2018-08-31 15:39:33 +0700

    Tests for ExampleTag

--- src/Tag/PhpDocumentor/ExampleTag.php
+++ src/Tag/PhpDocumentor/ExampleTag.php
@@ -6,6 +6,7 @@ namespace Jasny\Annotations\Tag\PhpDocumentor;
 
 use Jasny\Annotations\Tag\AbstractTag;
 use Jasny\Annotations\AnnotationException;
+use function Jasny\array_only;
 
 /**
  * Custom logic for PhpDocumentor 'example' tag
@@ -21,17 +22,25 @@ class ExampleTag extends AbstractTag
      */
     public function process(array $annotations, string $value): array
     {
-        $regexp = '/^(?:(?<location>[^"]\S*)|"(?<location>[^"]+)")'
-            . '(?:\s*(?<start_line>\d+)(?:\s*(?<number_of_lines>\d+))?)?/J';
+        $regexp = '/^(?<location>(?:[^"]\S*|"[^"]+"))(?:\s*(?<start_line>\d+)(?:\s*(?<number_of_lines>\d+))?)?/';
 
         if (!preg_match($regexp, $value, $matches)) {
             throw new AnnotationException("Failed to parse '@{$this->name} $value': invalid syntax");
         }
 
-        $matches['start_line'] = isset($matches['start_line']) ? (int)$matches['start_line'] : null;
-        $matches['number_of_lines'] = isset($matches['number_of_lines']) ? (int)$matches['number_of_lines'] : null;
+        $matches['location'] = trim($matches['location'], '"');
 
-        $annotations['example'] = $matches;
+        if (isset($matches['start_line'])) {
+            $matches['start_line'] = (int)$matches['start_line'];
+        }
+
+        if (isset($matches['number_of_lines'])) {
+            $matches['number_of_lines'] = (int)$matches['number_of_lines'];
+        }
+
+        $matches = array_only($matches, ['location', 'start_line', 'number_of_lines']);
+
+        $annotations[$this->name] = $matches;
 
         return $annotations;
     }
