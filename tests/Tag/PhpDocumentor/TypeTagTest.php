ff3e7ca44e9eed4faf60e65c0e0cd56551525ad0
Sergey Boltonosov <minstel@yandex.ru>
2018-09-10 22:37:29 +0700

    Added TypeTag (fixes #20)

---
+++ tests/Tag/PhpDocumentor/TypeTagTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Jasny\PhpdocParser\Tests\Tag\PhpDocumentor;
+
+use Jasny\PhpdocParser\Tag\PhpDocumentor\TypeTag;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Jasny\PhpdocParser\Tag\PhpDocumentor\TypeTag
+ * @covers \Jasny\PhpdocParser\Tag\AbstractTag
+ */
+class TypeTagTest extends TestCase
+{
+    /**
+     * Provide data for testing 'process' method
+     *
+     * @return array
+     */
+    public function processProvider()
+    {
+        return [
+            [
+                'FooType',
+                null,
+                ['some' => 'value', 'foo' => ['type' => 'FooType']]
+            ],
+            [
+                'FooType Some description here',
+                null,
+                ['some' => 'value', 'foo' => ['type' => 'FooType', 'description' => 'Some description here']]
+            ],
+            [
+                'Bar\\Foo\\Type',
+                null,
+                ['some' => 'value', 'foo' => ['type' => 'Bar\\Foo\\Type']]
+            ],
+            [
+                'Bar\\Foo\\Type Some description here',
+                null,
+                ['some' => 'value', 'foo' => ['type' => 'Bar\\Foo\\Type', 'description' => 'Some description here']]
+            ],
+            [
+                'FooType Some description here',
+                function($class) {
+                    return 'Bar\\' . $class;
+                },
+                ['some' => 'value', 'foo' => ['type' => 'Bar\\FooType', 'description' => 'Some description here']]
+            ],
+        ];
+    }
+
+    /**
+     * Test 'process' method
+     *
+     * @dataProvider processProvider
+     */
+    public function testProcess($value, $fqsenConvertor, $expected)
+    {
+        $tag = new TypeTag('foo', $fqsenConvertor);
+        $result = $tag->process(['some' => 'value'], $value);
+
+        $this->assertSame($expected, $result);
+    }
+
+    /**
+     * Test 'process' method, if exception should be thrown
+     *
+     * @expectedException Jasny\PhpdocParser\PhpdocException
+     * @expectedExceptionMessageRegExp /Failed to parse '@foo': tag value should not be empty/
+     */
+    public function testProcessEmptyValue()
+    {
+        $tag = new TypeTag('foo');
+        $result = $tag->process(['some' => 'value'], '');
+    }
+}
